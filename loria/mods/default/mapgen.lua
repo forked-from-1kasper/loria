309f5a1b4663e4909690ac0889bdcb6e0d7e9d90
Siegmentation <siegmentationfault@yandex.ru>
2019-06-28 20:23:50 +0700

    mushroom growning

--- loria/mods/default/mapgen.lua
+++ loria/mods/default/mapgen.lua
@@ -1,88 +1,4 @@
-minetest.register_alias("mapgen_stone", "default:cinnabar")
-minetest.register_alias("mapgen_dirt", "default:mercury_oxide")
-minetest.register_alias("mapgen_dirt_with_grass", "default:red_mercury_oxide")
-minetest.register_alias("mapgen_sand", "default:lead_sulfate")
-minetest.register_alias("mapgen_water_source", "default:mercury_source")
-minetest.register_alias("mapgen_river_water_source", "default:mercury_source")
-minetest.register_alias("mapgen_lava_source", "default:mercury_source")
-minetest.register_alias("mapgen_gravel", "default:uranium_tetrachloride_ore")
-
-minetest.register_alias("mapgen_tree", "default:viridi_petasum_stem")
-minetest.register_alias("mapgen_leaves", "default:viridi_petasum_body")
-minetest.register_alias("mapgen_apple", "default:cinnabar")
-minetest.register_alias("mapgen_junglegrass", "default:cinnabar")
-
-minetest.register_alias("mapgen_cobble", "default:cinnabar")
-minetest.register_alias("mapgen_stair_cobble", "default:cinnabar")
-minetest.register_alias("mapgen_mossycobble", "default:cinnabar")
-
-minetest.clear_registered_biomes()
-minetest.clear_registered_decorations()
-
-minetest.register_biome({
-    name = "default:redland",
-    node_top = "default:red_mercury_oxide",
-    depth_top = 1,
-    node_filler = "default:mercury_oxide",
-    depth_filler = 1,
-    y_min = 5,
-    y_max = 31000,
-    heat_point = 50,
-    humidity_point = 50,
-})
-
-minetest.register_biome({
-    name = "default:azure",
-    node_top = "default:copper_sulfate",
-    depth_top = 2,
-    node_stone = "default:cobalt_blue",
-    y_min = -31000,
-    y_max = 31000,
-    heat_point = 30,
-    humidity_point = 50,
-})
-
-minetest.register_biome({
-    name = "default:purple_swamp",
-    node_stone = "default:ammonium_manganese_pyrophosphate",
-
-    y_min = -15,
-    y_max = 31000,
-    heat_point = 40,
-    humidity_point = 50,
-})
-
-minetest.register_biome({
-    name = "default:swamp_connector",
-    node_stone = "air",
-    y_min = -20,
-    y_max = -15,
-    heat_point = 40,
-    humidity_point = 50,
-})
-
-minetest.register_biome({
-    name = "default:swamp_bottom",
-    node_stone = "default:potassium_permanganate_source",
-    y_min = -25,
-    y_max = -20,
-    heat_point = 40,
-    humidity_point = 50,
-})
-
-minetest.register_biome({
-    name = "default:mercury_ocean",
-    node_top = "default:lead_sulfate",
-    depth_top = 1,
-    node_filler = "default:lead_sulfate",
-    depth_filler = 2,
-    y_min = -31000,
-    y_max = 4,
-    heat_point = 50,
-    humidity_point = 50,
-})
-
-for i = 1, #pars_names do
+for i, _ in ipairs(pars_names) do
     minetest.register_decoration({
         deco_type = "simple",
         place_on = "default:copper_sulfate",
@@ -90,34 +6,6 @@ for i = 1, #pars_names do
         fill_ratio = 0.1,
         biomes = "default:azure",
         decoration = "default:pars_" .. i,
-        height = 1
-    })
-end
-
-for _, name in ipairs({ "pusilli", "caput", "quercu" }) do
-    minetest.register_decoration({
-        deco_type = "simple",
-        place_on = {
-            "default:cinnabar",
-            "default:red_mercury_oxide",
-            "default:mercury_oxide"
-        },
-        sidelen = 16,
-        fill_ratio = 0.01,
-        biomes = "default:redland",
-        decoration = "default:" .. name,
-        height = 1
-    })
-end
-
-for _, name in ipairs({ "rosea", "picea", "purpura" }) do
-    minetest.register_decoration({
-        deco_type = "simple",
-        place_on = "default:copper_sulfate",
-        sidelen = 16,
-        fill_ratio = 0.05,
-        biomes = "default:azure",
-        decoration = "default:" .. name,
         height = 1
     })
 end
@@ -131,24 +19,12 @@ for _, name in ipairs({ "rami", "spears" }) do
         biomes = "default:purple_swamp",
         decoration = "default:" .. name,
         height = 1,
-        y_max = 4
-    })
-end
-
-for _, name in ipairs({ "periculum", "grebe", "secreta" }) do
-    minetest.register_decoration({
-        deco_type = "simple",
-        place_on = "default:ammonium_manganese_pyrophosphate",
-        sidelen = 16,
-        fill_ratio = 0.01,
-        biomes = "default:purple_swamp",
-        decoration = "default:" .. name,
-        height = 1
+        height_max = 4
     })
 end
 
 max_truncus_height = 7
-for i = 1, #truncus_names do
+for i, _ in ipairs(truncus_names) do
     minetest.register_decoration({
         deco_type = "simple",
         place_on = "default:copper_sulfate",
@@ -157,7 +33,7 @@ for i = 1, #truncus_names do
         biomes = "default:azure",
         decoration = "default:truncus_" .. i,
         height = 1,
-        y_max = max_truncus_height
+        height_max = max_truncus_height
     })
 end
 
--- loria/mods/default/small_mushrooms.lua
+++ loria/mods/default/small_mushrooms.lua
@@ -64,16 +64,76 @@ minetest.register_node("default:naga", {
 })
 
 small_mushrooms = {
-    ["pusilli"] = { damage = 5 },
-    ["rosea"] = { damage = 4 },
-    ["purpura"] = { features = { light_source = 5 } },
-    ["picea"] = { damage = 9 },
-    ["caput"] = { damage = 15 },
-    ["periculum"] = { features = { light_source = 3 }, damage = 18 },
+    ["pusilli"] = {
+        damage = 5,
+        place_on = {
+            "default:cinnabar",
+            "default:red_mercury_oxide",
+            "default:mercury_oxide"
+        },
+        optimal_light = 11,
+        fill_ratio = 0.01,
+        biomes = "default:redland",
+    },
+    ["rosea"] = {
+        damage = 4,
+        place_on = "default:copper_sulfate",
+        fill_ratio = 0.05,
+        biomes = "default:azure",
+    },
+    ["purpura"] = {
+        features = { light_source = 5 },
+        place_on = "default:copper_sulfate",
+        fill_ratio = 0.05,
+        biomes = "default:azure",
+    },
+    ["picea"] = {
+        damage = 9,
+        place_on = "default:copper_sulfate",
+        fill_ratio = 0.05,
+        biomes = "default:azure",
+    },
+    ["caput"] = {
+        damage = 15,
+        place_on = {
+            "default:cinnabar",
+            "default:red_mercury_oxide",
+            "default:mercury_oxide"
+        },
+        optimal_light = 11,
+        fill_ratio = 0.01,
+        biomes = "default:redland",
+    },
+    ["periculum"] = {
+        features = { light_source = 3 },
+        damage = 18,
+        place_on = "default:ammonium_manganese_pyrophosphate",
+        fill_ratio = 0.01,
+        biomes = "default:purple_swamp",
+    },
     ["vastatorem"] = { features = { light_source = 8 } },
-    ["quercu"] = { damage = 7 },
-    ["grebe"] = { damage = 11 },
-    ["secreta"] = { damage = 12 },
+    ["quercu"] = {
+        damage = 7,
+        place_on = {
+            "default:cinnabar",
+            "default:red_mercury_oxide",
+            "default:mercury_oxide"
+        },
+        optimal_light = 11,
+        biomes = "default:redland",
+    },
+    ["grebe"] = {
+        damage = 11,
+        place_on = "default:ammonium_manganese_pyrophosphate",
+        fill_ratio = 0.01,
+        biomes = "default:purple_swamp",
+    },
+    ["secreta"] = {
+        damage = 12,
+        place_on = "default:ammonium_manganese_pyrophosphate",
+        fill_ratio = 0.01,
+        biomes = "default:purple_swamp",
+    },
     ["pulchram"] = { damage = 3 },
     ["conc"] = { damage = 16 }
 }
@@ -102,4 +162,39 @@ for name, params in pairs(small_mushrooms) do
     end
 
     minetest.register_node("default:" .. name, info)
+    minetest.register_abm({
+        label = name:gsub("^%l", string.upper) .. " spread",
+        nodenames = { "default:" .. name },
+        interval = 11,
+        chance = 150,
+        action = function(pos)
+            local positions = minetest.find_nodes_in_area_under_air(
+                { x = pos.x - 1, y = pos.y - 2, z = pos.z - 1 },
+                { x = pos.x + 1, y = pos.y + 1, z = pos.z + 1 },
+                params.place_on
+            )
+
+            if #positions == 0 then
+                return
+            end
+
+            local pos2 = positions[math.random(#positions)]
+            pos2.y = pos2.y + 1
+            if minetest.get_node_light(pos) <= (params.optimal_light or 7) then
+                minetest.set_node(pos2, { name = "default:" .. name })
+            end
+        end
+    })
+
+    if params.place_on and params.fill_ratio and params.biomes then
+        minetest.register_decoration({
+            deco_type = "simple",
+            place_on = params.place_on,
+            sidelen = 16,
+            fill_ratio = params.fill_ratio,
+            biomes = params.biomes,
+            decoration = "default:" .. name,
+            height = 1
+        })
+    end
 end
