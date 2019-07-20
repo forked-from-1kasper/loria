162320e094fd6078c379ea80b04d5447c7b0eb5c
Siegmentation <siegmentationfault@yandex.ru>
2019-07-20 20:09:48 +0700

    fix

--- loria/mods/default/nodes.lua
+++ loria/mods/default/nodes.lua
@@ -180,7 +180,7 @@ minetest.register_node("default:glow_stick", {
     sunlight_propagates = true,
     is_ground_content = false,
     walkable = false,
-    light_source = 1,
+    light_source = 10,
     selection_box = {
         type = "wallmounted",
         wall_top = {-0.1, 0.5-0.6, -0.1, 0.1, 0.5, 0.1},
