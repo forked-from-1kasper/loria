ccb037a709dd92f33c7a80c0dc58b036196f9e50
Siegmentation <siegmentationfault@yandex.ru>
2019-07-23 17:46:55 +0700

    lost file

---
+++ loria/mods/default/sky.lua
@@ -0,0 +1,59 @@
+local default_color = { r = 140, g = 186, b = 250 }
+local night_color = { r = 0, g = 0, b = 16 }
+
+local colors = {
+    [minetest.get_biome_id("default:redland")] = {r = 255, g = 200, b = 150 },
+    [minetest.get_biome_id("default:purple_swamp")] = { r = 190, g = 105, b = 255 },
+}
+
+local sunrise = { start = 4500/24000, finish = 5751/24000 }
+local sunset = { start = 18600/24000, finish = 19502/24000 }
+
+local function animation_curve(x)
+    return math.sin(x) + (1 - math.sin(1))
+end
+
+local function brightness(color, x)
+    return {
+        r = x * color.r,
+        g = x * color.g,
+        b = x * color.b,
+    }
+end
+
+local function addition(c1, c2)
+    return {
+        r = math.min(c1.r + c2.r, 255),
+        g = math.min(c1.g + c2.g, 255),
+        b = math.min(c1.b + c2.b, 255),
+    }
+end
+
+local function get_sky_color(color, timeofday)
+    if timeofday < sunrise.start then
+        return brightness(color, 0)
+    elseif timeofday >= sunrise.start and timeofday < sunrise.finish then
+        return brightness(color,
+            (timeofday - sunrise.start) / (sunrise.finish - sunrise.start)
+        )
+    elseif timeofday >= sunrise.finish and timeofday < sunset.start then
+        return brightness(color, 1)
+    elseif timeofday >= sunset.start and timeofday < sunset.finish then
+        return brightness(color,
+            (sunset.finish - timeofday) / (sunset.finish - sunset.start)
+        )
+    elseif timeofday >= sunset.finish then
+        return brightness(color, 0)
+    end
+end
+
+minetest.register_globalstep(function(dtime)
+    local timeofday = minetest.get_timeofday()
+
+    for _, player in ipairs(minetest.get_connected_players()) do
+        local pos = player:get_pos()
+        local color = colors[minetest.get_biome_data(pos).biome] or default_color
+
+        player:set_sky(addition(get_sky_color(color, timeofday), night_color), "plain")
+    end
+end)
